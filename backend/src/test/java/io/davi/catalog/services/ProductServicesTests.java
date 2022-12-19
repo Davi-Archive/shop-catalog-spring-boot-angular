9fe281a4ef3f1bee407768f7a1f30369750281f2
d1av <davi4alves@gmail.com>
2022-12-19 20:05:56 -0300

    feat: add delete method should throw

--- backend/src/test/java/io/davi/catalog/services/ProductServicesTests.java
+++ backend/src/test/java/io/davi/catalog/services/ProductServicesTests.java
@@ -1,6 +1,7 @@
 package io.davi.catalog.services;
 
 import io.davi.catalog.repositories.ProductRepository;
+import io.davi.catalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -18,10 +20,15 @@ public class ProductServicesTests {
     private ProductRepository repository;
 
     private Long existingId;
+    private Long nonExistingId;
 
     @BeforeEach
     void setUp() throws Exception {
         existingId = 1L;
+        nonExistingId = 1000L;
+
+        Mockito.doNothing().when(repository).deleteById(existingId);
+        Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
     }
 
     @Test
@@ -30,6 +37,15 @@ public class ProductServicesTests {
             service.delete(existingId);
         });
 
-        Mockito.verify(repository,Mockito.times(1)).deleteById(existingId);
+        Mockito.verify(repository, Mockito.times(1)).deleteById(existingId);
+    }
+
+    @Test
+    public void deleteShouldThrowWhenIdDoesNotExist() {
+        Assertions.assertThrows(ResourceNotFoundException.class,() -> {
+            service.delete(nonExistingId);
+        });
+
+        Mockito.verify(repository, Mockito.times(1)).deleteById(nonExistingId);
     }
 }
