a68a76f48c478d27044fb8f3a118250546ba607c
d1av <davi4alves@gmail.com>
2022-12-18 21:07:45 -0300

    feat: Post category with localization header

--- backend/src/main/java/io/davi/catalog/resources/CategoryResource.java
+++ backend/src/main/java/io/davi/catalog/resources/CategoryResource.java
@@ -2,13 +2,13 @@ package io.davi.catalog.resources;
 
 import io.davi.catalog.dto.CategoryDTO;
 import io.davi.catalog.services.CategoryService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +28,16 @@ public class CategoryResource {
     public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
         CategoryDTO category = service.findById(id);
         return ResponseEntity.ok().body(category);
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+        dto = service.insert(dto);
+
+        // location header
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
