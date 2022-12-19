378676334b82756bd788ae99abf950480d9a443b
d1av <davi4alves@gmail.com>
2022-12-19 19:08:40 -0300

    feat: add test for product resource

---
+++ backend/src/test/java/io/davi/catalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,36 @@
+package io.davi.catalog.repositories;
+
+
+import io.davi.catalog.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists() {
+        Long existingId = 1L;
+
+        repository.deleteById(existingId);
+
+        Optional<Product> result = repository.findById(existingId);
+        Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist(){
+        Long invalidId = 121421414451111L;
+        Assertions.assertThrows(EmptyResultDataAccessException.class,()->{
+           repository.deleteById(invalidId);
+        });
+    }
+}
