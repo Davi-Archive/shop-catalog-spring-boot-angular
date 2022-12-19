d07d616205763dd5ae5bca2e636a51d88c03b5e6
d1av <davi4alves@gmail.com>
2022-12-18 23:08:44 -0300

    feat: add product insert,update,delete

--- backend/src/main/java/io/davi/catalog/services/ProductService.java
+++ backend/src/main/java/io/davi/catalog/services/ProductService.java
@@ -1,7 +1,10 @@
 package io.davi.catalog.services;
 
+import io.davi.catalog.dto.CategoryDTO;
 import io.davi.catalog.dto.ProductDTO;
+import io.davi.catalog.entities.Category;
 import io.davi.catalog.entities.Product;
+import io.davi.catalog.repositories.CategoryRepository;
 import io.davi.catalog.repositories.ProductRepository;
 import io.davi.catalog.services.exceptions.DatabaseException;
 import io.davi.catalog.services.exceptions.ResourceNotFoundException;
@@ -23,6 +26,9 @@ public class ProductService {
 
     @Autowired
     private ProductRepository repository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
     public List<ProductDTO> findAll() {
@@ -58,7 +64,7 @@ public class ProductService {
     @Transactional
     public ProductDTO insert(ProductDTO dto) {
         Product entity = new Product();
-        // entity.setName(dto.getName());
+        copyDtoToEntity(dto, entity);
         entity = repository.save(entity);
         return new ProductDTO(entity);
     }
@@ -67,7 +73,7 @@ public class ProductService {
     public ProductDTO update(Long id, ProductDTO dto) {
         try {
             Product entity = repository.getOne(id);
-            //   entity.setName(dto.getName());
+            copyDtoToEntity(dto, entity);
             entity = repository.save(entity);
             return new ProductDTO(entity);
         } catch (EntityNotFoundException e) {
@@ -85,5 +91,18 @@ public class ProductService {
         }
     }
 
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setDate(dto.getDate());
+        entity.setImgUrl(dto.getImgUrl());
+        entity.setPrice(dto.getPrice());
+
+        entity.getCategories().clear();
+        for (CategoryDTO catDto : dto.getCategories()) {
+            Category category = categoryRepository.getOne(catDto.getId());
+            entity.getCategories().add(category);
+        }
+    }
 
 }
