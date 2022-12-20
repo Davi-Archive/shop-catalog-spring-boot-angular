801faf601be4d5b40cd0308153e09a7c01b465a7
d1av <davi4alves@gmail.com>
2022-12-19 22:10:07 -0300

    refactor: better test readability

--- backend/src/test/java/io/davi/catalog/resources/ProductResourcesTests.java
+++ backend/src/test/java/io/davi/catalog/resources/ProductResourcesTests.java
@@ -11,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.List;
@@ -39,9 +41,11 @@ public class ProductResourcesTests {
     }
 
     @Test
-    public void findAllShouldReturnPage() throws Exception{
-        mockMvc.perform(get("/products"))
-                .andExpect(status().isOk());
+    public void findAllShouldReturnPage() throws Exception {
+        ResultActions result =
+                mockMvc.perform(get("/products")
+                        .accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isOk());
     }
 
 }
