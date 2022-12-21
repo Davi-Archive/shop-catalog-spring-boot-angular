d543754b111c7805a9f7f63179bf088a0700ee51
d1av <davi4alves@gmail.com>
2022-12-20 23:45:53 -0300

    feat: add user update validator with same email

--- backend/src/main/java/io/davi/catalog/services/validation/UserUpdateValid.java
+++ backend/src/main/java/io/davi/catalog/services/validation/UserUpdateValid.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Constraint(validatedBy = UserInsertValidator.class)
+@Constraint(validatedBy = UserUpdateValidator.class)
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UserUpdateValid {
--- backend/src/main/java/io/davi/catalog/services/validation/UserUpdateValidator.java
+++ backend/src/main/java/io/davi/catalog/services/validation/UserUpdateValidator.java
@@ -1,17 +1,25 @@
 package io.davi.catalog.services.validation;
 
 import io.davi.catalog.dto.UserInsertDTO;
+import io.davi.catalog.dto.UserUpdateDTO;
 import io.davi.catalog.entities.User;
 import io.davi.catalog.repositories.UserRepository;
 import io.davi.catalog.resources.exceptions.FieldMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Handler;
 
-public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserInsertDTO> {
+public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
+
+    @Autowired
+    private HttpServletRequest request;
 
     @Autowired
     private UserRepository repository;
@@ -21,16 +29,19 @@ public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid,
     }
 
     @Override
-    public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+    public boolean isValid(UserUpdateDTO dto, ConstraintValidatorContext context) {
+
+        @SuppressWarnings("unchecked")
+        var uriVars = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        Long userId = Long.parseLong(uriVars.get("id"));
 
         List<FieldMessage> list = new ArrayList<>();
 
         User user = repository.findByEmail(dto.getEmail());
         // Coloque aqui seus testes de validação, acrescentando objetos FieldMessage à lista
-        if(user !=null){
-            list.add(new FieldMessage("email","Email já existe."));
+        if (user != null && !userId.equals(user.getId())) {
+            list.add(new FieldMessage("email", "Email já existe."));
         }
-
 
 
         for (FieldMessage e : list) {
