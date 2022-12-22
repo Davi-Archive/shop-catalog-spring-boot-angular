4145ad13a0b8900c16b10c89d83be40eb45842bb
d1av <davi4alves@gmail.com>
2022-12-21 22:29:29 -0300

    feat: implemented variables for OAuth2

--- backend/src/main/java/io/davi/catalog/config/AppConfig.java
+++ backend/src/main/java/io/davi/catalog/config/AppConfig.java
@@ -1,13 +1,19 @@
 package io.davi.catalog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import javax.validation.Valid;
+
 @Configuration
 public class AppConfig {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -18,7 +24,7 @@ public class AppConfig {
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey("MY-JWT-SECRET");
+        tokenConverter.setSigningKey(jwtSecret);
         return tokenConverter;
     }
 
