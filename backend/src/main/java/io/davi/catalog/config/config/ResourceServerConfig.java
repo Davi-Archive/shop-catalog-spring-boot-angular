b9c288b1f5a95d36d26417ab9189c0a2c6cdb88f
d1av <davi4alves@gmail.com>
2022-12-21 23:12:00 -0300

    feat: h2 security configuration

--- backend/src/main/java/io/davi/catalog/config/config/ResourceServerConfig.java
+++ backend/src/main/java/io/davi/catalog/config/config/ResourceServerConfig.java
@@ -3,6 +3,7 @@ package io.davi.catalog.config.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -10,14 +11,19 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
+    private Environment env;
+
+    @Autowired
     private JwtTokenStore tokenStore;
 
-    private static final String[] PUBLIC = {"/oauth/token"};
+    private static final String[] PUBLIC = {"/oauth/token", "/h2/**"};
 
     private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
 
@@ -31,11 +37,18 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-            .antMatchers(PUBLIC).permitAll()
-            .antMatchers(HttpMethod.GET,OPERATOR_OR_ADMIN).permitAll()
-            .antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR","ADMIN")
-            .antMatchers(ADMIN).hasRole("ADMIN")
-            .anyRequest().authenticated();
+
+
+        // Unblock H2
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
+        http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
+                .antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
+                .antMatchers(ADMIN).hasRole("ADMIN")
+                .anyRequest().authenticated();
     }
 }
