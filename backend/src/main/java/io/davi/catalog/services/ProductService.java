c44da637bba616b4f3e7db755e7b223af6a72e79
d1av <davi4alves@gmail.com>
2022-12-23 22:34:06 -0300

    feat: implemented query with treatment

--- backend/src/main/java/io/davi/catalog/services/ProductService.java
+++ backend/src/main/java/io/davi/catalog/services/ProductService.java
@@ -49,9 +49,9 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAllPaged(Long categoryId, Pageable pageable) {
+    public Page<ProductDTO> findAllPaged(Long categoryId, String name, Pageable pageable) {
         Category category = (categoryId == 0) ? null : categoryRepository.getOne(categoryId);
-        Page<Product> list = repository.find(category, pageable);
+        Page<Product> list = repository.find(category, name, pageable);
         return list.map(x -> new ProductDTO(x));
     }
 
