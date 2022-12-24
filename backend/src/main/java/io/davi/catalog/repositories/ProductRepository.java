a1682fdea52bc39a9d323c3c93d56e41b19ed0d5
d1av <davi4alves@gmail.com>
2022-12-23 22:41:25 -0300

    fix: postgres compatibility

--- backend/src/main/java/io/davi/catalog/repositories/ProductRepository.java
+++ backend/src/main/java/io/davi/catalog/repositories/ProductRepository.java
@@ -8,10 +8,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE " +
-            "(:category IS NULL OR :category IN cats) AND "
-            + "(LOWER(obj.name) LIKE LOWER( CONCAT('%', :name ,'%'))  )")
-    Page<Product> find(Category category, String name, Pageable pageable);
+            "(COALESCE(:categories) IS NULL OR cats IN :categories) AND "
+            + "(:name= '' OR LOWER(obj.name) LIKE LOWER( CONCAT('%', :name ,'%'))  )")
+    Page<Product> find(List<Category> categories, String name, Pageable pageable);
 }
