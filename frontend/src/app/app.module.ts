653ac933e6ce4b99cd08d4be89035e68b5c342a9
d1av <davi4alves@gmail.com>
2022-12-25 01:47:46 -0300

    feat: catalog page implemented

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComponentsModule } from './shared/components.module';
+import { PagesModule } from './pages/pages.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+    ],
   imports: [
+    RouterModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ComponentsModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/pages/pages.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { AdminComponent } from './admin/admin.component';
+import { ComponentsModule } from '../shared/components.module';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    LoginComponent,
+    CatalogComponent,
+    AdminComponent
+  ],
+  imports: [
+    CommonModule,
+    ComponentsModule
+  ],
+  exports:[
+    HomeComponent,
+    LoginComponent
+  ]
+})
+export class PagesModule { }
