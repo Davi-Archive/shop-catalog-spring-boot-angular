62d69dcf25507f6c1cc6318399e5f3db18085b09
d1av <davi4alves@gmail.com>
2022-12-26 17:57:36 -0300

    feat: add fonts and variables

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,5 @@
-import '../styles/globals.css';
+import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
-import 'bootstrap/dist/css/bootstrap.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ frontend/shared/components/index.tsx
@@ -0,0 +1,5 @@
+import Navbar from "./layout/Navbar"
+
+export {
+    Navbar
+}
