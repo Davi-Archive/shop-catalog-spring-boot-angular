2a922a2d088a81a599ef179772924b33d32591c1
d1av <davi4alves@gmail.com>
2022-12-27 12:16:46 -0300

    feat: add bucket support

--- backend/src/main/java/io/davi/catalog/CatalogApplication.java
+++ backend/src/main/java/io/davi/catalog/CatalogApplication.java
@@ -1,13 +1,25 @@
 package io.davi.catalog;
 
+import com.amazonaws.services.s3.AmazonS3;
+import io.davi.catalog.services.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatalogApplication {
+public class CatalogApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CatalogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogApplication.class, args);
+    }
 
+
+    @Autowired
+    private S3Service s3Service;
+
+    @Override
+    public void run(String... args) throws Exception {
+        s3Service.uploadFile("c:\\img.jpg");
+    }
 }
