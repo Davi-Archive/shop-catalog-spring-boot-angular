c663ee0f284cf578a7827236dd91dcf3b37ca821
d1av <davi4alves@gmail.com>
2022-12-26 21:55:39 -0300

    feat: product details styling

--- frontend/src/app/pages/products/product-detail/product-detail.component.ts
+++ frontend/src/app/pages/products/product-detail/product-detail.component.ts
@@ -13,7 +13,7 @@ export class ProductDetailComponent {
     "name": "Macbook Pro",
     "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     "price": 1250.0,
-    "imgUrl": "https://rw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg",
+    "imgUrl": "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg",
     "date": "2020-07-14T10:00:00Z",
     "categories": [
       {
